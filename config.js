9bfc86a406292a5be6cd8da07fc4d88eee35956c
Ivan Mironchik <me.roaiven@gmail.com>
2016-07-26 11:11:01 +0300

    Add mailgun config

--- config.js
+++ config.js
@@ -3,7 +3,16 @@ var path = require('path'),
 module.exports = {
     production: {
         url: process.env.HEROKU_URL,
-        mail: {},
+        mail: {
+            transport: 'SMTP',
+            options: {
+            service: 'Mailgun',
+                auth: {
+                    user: process.env.MAILGUN_SMTP_LOGIN,
+                    pass: process.env.MAILGUN_SMTP_PASSWORD
+                }
+            }
+        },
         database: {
             client: 'postgres',
             connection: process.env.DATABASE_URL,
